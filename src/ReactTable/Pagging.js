e1be75c607e8f4ab7a222c9ea3aa7e0d5e84bde6
errahmiayoub <ayoub_errahmi@um5.ac.ma>
2021-01-24 02:53:37 +0100

    responsive update

--- src/ReactTable/Pagging.js
+++ src/ReactTable/Pagging.js
@@ -2,6 +2,7 @@ import React from "react";
 import IconButton from "@material-ui/core/IconButton";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 
 import FirstPageIcon from "@material-ui/icons/FirstPage";
@@ -12,9 +13,11 @@ import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
 const useStyles = makeStyles((theme) => ({
   container: {
     width: "100%",
+  },
+  item: {
     display: "flex",
     justifyContent: "center",
-    alignItems: "center",
+    whiteSpace: "nowrap",
   },
   margin: {
     margin: theme.spacing(1),
@@ -40,70 +43,81 @@ const Pagging = (props) => {
     setPageSize,
   } = props;
   return (
-    <div className={classes.container}>
-      <IconButton
-        className={classes.margin}
-        onClick={() => gotoPage(0)}
-        disabled={!canPreviousPage}
-      >
-        <FirstPageIcon />
-      </IconButton>
-      <IconButton
-        className={classes.margin}
-        onClick={() => previousPage()}
-        disabled={!canPreviousPage}
-      >
-        <NavigateBeforeIcon />
-      </IconButton>
-      <IconButton
-        className={classes.margin}
-        onClick={() => nextPage()}
-        disabled={!canNextPage}
-      >
-        <NavigateNextIcon />
-      </IconButton>
-      <IconButton
-        className={classes.margin}
-        onClick={() => gotoPage(pageCount - 1)}
-        disabled={!canNextPage}
-      >
-        <LastPageIcon />
-      </IconButton>
-      <span className={classes.margin}>
-        Page{" "}
-        <strong>
-          {pageIndex + 1} of {pageOptions.length}
-        </strong>
-      </span>
-      <span className={classes.margin}>| Go to page: </span>
-      <TextField
-        className={classes.margin}
-        type="number"
-        defaultValue={pageIndex + 1}
-        onChange={(e) => {
-          const page = e.target.value ? Number(e.target.value) - 1 : 0;
-          gotoPage(page);
-        }}
-        style={{ width: 150 }}
-        variant="outlined"
-        size="small"
-        label="Page"
-      />
-      <Autocomplete
-        id="filter"
-        options={[5, 10, 20, 30, 40, 50]}
-        size="small"
-        getOptionLabel={(option) => `Show ${option}`}
-        style={{ width: 150 }}
-        renderInput={(params) => (
-          <TextField {...params} label="pageSize" variant="outlined" />
-        )}
-        value={pageSize}
-        onChange={(e, v) => {
-          if (Number(v)) setPageSize(Number(v));
-        }}
-      />
-    </div>
+    <Grid
+      container
+      alignItems="center"
+      justify="center"
+      className={classes.container}
+    >
+      <Grid className={classes.item} item xs={12} sm>
+        <IconButton
+          className={classes.margin}
+          onClick={() => gotoPage(0)}
+          disabled={!canPreviousPage}
+        >
+          <FirstPageIcon />
+        </IconButton>
+        <IconButton
+          className={classes.margin}
+          onClick={() => previousPage()}
+          disabled={!canPreviousPage}
+        >
+          <NavigateBeforeIcon />
+        </IconButton>
+        <IconButton
+          className={classes.margin}
+          onClick={() => nextPage()}
+          disabled={!canNextPage}
+        >
+          <NavigateNextIcon />
+        </IconButton>
+        <IconButton
+          className={classes.margin}
+          onClick={() => gotoPage(pageCount - 1)}
+          disabled={!canNextPage}
+        >
+          <LastPageIcon />
+        </IconButton>
+      </Grid>
+      <Grid item xs={12} sm className={classes.item}>
+        <span className={classes.margin}>
+          Page{" "}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>
+        </span>
+        <span className={classes.margin}>| Go to page: </span>
+        <TextField
+          className={classes.margin}
+          type="number"
+          defaultValue={pageIndex + 1}
+          onChange={(e) => {
+            const page = e.target.value ? Number(e.target.value) - 1 : 0;
+            gotoPage(page);
+          }}
+          style={{ width: 150 }}
+          variant="outlined"
+          size="small"
+          label="Page"
+        />
+      </Grid>
+      <Grid item xs={12} sm className={classes.item}>
+        <Autocomplete
+          id="filter"
+          options={[5, 10, 20, 30, 40, 50]}
+          size="small"
+          getOptionLabel={(option) => `Show ${option}`}
+          style={{ width: 150 }}
+          renderInput={(params) => (
+            <TextField {...params} label="pageSize" variant="outlined" />
+          )}
+          value={pageSize}
+          onChange={(e, v) => {
+            if (Number(v)) setPageSize(Number(v));
+          }}
+        />
+      </Grid>
+    </Grid>
   );
 };
 
